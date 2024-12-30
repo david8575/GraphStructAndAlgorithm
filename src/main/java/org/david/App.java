679feaf55e0b158a4c18676f2f1b871de2395565
Park SeHun <103345490+david8575@users.noreply.github.com>
2024-12-30 16:26:49 +0900

    Directed Graph Updated

--- src/main/java/org/david/App.java
+++ src/main/java/org/david/App.java
@@ -1,10 +1,39 @@
 package org.david;
 
+import org.david.Graph.DirectedGraph.DirectedGraph;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        DirectedGraph graph = new DirectedGraph();
+
+        graph.addVertex(1, "A");
+        graph.addVertex(2, "B");
+        graph.addVertex(3, "C");
+        graph.addVertex(4, "D");
+        graph.addVertex(5, "E");
+        graph.addVertex(6, "F");
+        graph.addVertex(7, "G");
+        graph.addVertex(8, "H");
+
+
+        graph.addEdge(1, 2, 5);
+        graph.addEdge(2, 3, 3);
+        graph.addEdge(1, 3, 10);
+        graph.addEdge(3, 4, 5);
+        graph.addEdge(4, 5, 6);
+        graph.addEdge(5, 6, 7);
+        graph.addEdge(6, 7, 8);
+        graph.addEdge(7, 8, 9);
+        graph.addEdge(8, 1, 10);
+        graph.addEdge(1, 3, 11);
+        graph.addEdge(1, 5, 12);
+        graph.addEdge(1, 6, 13);
+        graph.addEdge(1, 7, 14);
+        graph.addEdge(1, 1, 15);
+
+        graph.printGraph();
     }
 }
